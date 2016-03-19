325ef028e95db57851553548037ed4dc497338d0
alinabalaban <alina_irina_b@yahoo.com>
2016-03-19 12:17:11 +0200

    tema

---
+++ src/geometrical/GeometricalFigure.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometrical;
+
+/**
+ *
+ * @author Alina
+ */
+public abstract class GeometricalFigure {
+     protected Origin O;
+    abstract void draw();
+    
+}
---
+++ src/geometrical/Origin.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geometrical;
+
+/**
+ *
+ * @author Alina
+ */
+public class Origin {
+    int X;
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int X) {
+        this.X = X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int Y) {
+        this.Y = Y;
+    }
+    int Y;
+    
+}
